5abe361c3f6c7467ae42d7f64036d9b108554412
aries-rst <68280428+aries-rst@users.noreply.github.com>
2026-09-26 11:56:54 +0500

    Integrate StoreManager into MyOfficeApp
    Added StoreManager as a state object and initialized it in the app.

--- MyOffice/App/MyOfficeApp.swift
+++ MyOffice/App/MyOfficeApp.swift
@@ -3,11 +3,16 @@ import SwiftUI
 @main
 struct MyOfficeApp: App {
     @StateObject private var appState = AppState()
+    @StateObject private var store = StoreManager()
 
     var body: some Scene {
         WindowGroup {
             RootTabView()
                 .environmentObject(appState)
+                .environmentObject(store)
+                .task {
+                    store.start(appState: appState)
+                }
         }
     }
 }
